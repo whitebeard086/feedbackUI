12c4c463945fc06959af97fbb3b9e4d6987d62f0
unknown <whitebeard086@gmail.com>
2022-03-31 15:03:14 +0100

    implemented feedback delete

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import { FeedbackList, Header } from "./components";
 import FeedbackData from "./data/feedbackData";
 
 function App() {
-    const [feedback, setFeedback] = useState(FeedbackData)
+  const [feedback, setFeedback] = useState(FeedbackData);
+
+  const deleteFeedback = id => {
+    if (window.confirm("Are you sure you want to delete this feedback?")) {
+      setFeedback(feedback.filter(item => item.id !== id));
+    }
+  };
 
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackList feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   );
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,17 +1,22 @@
+import { FaTimes } from "react-icons/fa";
 import PropTypes from "prop-types";
 import Card from "./shared/Card";
 
-const FeedbackItem = ({ item }) => {
+const FeedbackItem = ({ item, handleDelete }) => {
+
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
+      <button onClick={() => handleDelete(item.id)} className="close">
+        <FaTimes />
+      </button>
       <div className="text-display">{item.text}</div>
     </Card>
   );
 };
 
 FeedbackItem.propTypes = {
-    item: PropTypes.object.isRequired,
-}
+  item: PropTypes.object.isRequired,
+};
 
 export default FeedbackItem;
