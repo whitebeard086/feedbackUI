235c3b56c72be3004a80eb39ce39da149d2fb532
unknown <whitebeard086@gmail.com>
2022-04-01 08:05:56 +0100

    added animation for adding and removing items from list

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,5 @@
 import PropTypes from "prop-types";
+import { motion, AnimatePresence } from "framer-motion";
 import FeedbackItem from "./FeedbackItem";
 
 const FeedbackList = ({ feedback, handleDelete }) => {
@@ -8,20 +9,37 @@ const FeedbackList = ({ feedback, handleDelete }) => {
 
   return (
     <div className="feedback-list">
-      {feedback?.map(item => (
-        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
-      ))}
+      <AnimatePresence>
+        {feedback?.map(item => (
+          <motion.div 
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem item={item} handleDelete={handleDelete} />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
+
+  // return (
+  //   <div className="feedback-list">
+  //     {feedback?.map(item => (
+  //       <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+  //     ))}
+  //   </div>
+  // );
 };
 
 FeedbackList.propTypes = {
   feedback: PropTypes.arrayOf(
-      PropTypes.shape({
-          id: PropTypes.number.isRequired,
-          text: PropTypes.string.isRequired,
-          rating: PropTypes.number.isRequired,
-      })
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
   ),
 };
 
